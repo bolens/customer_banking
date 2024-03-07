f98359bf9bb0fa683d287dfd644c309d03fc5402
bolens <bolens@duck.com>
2024-03-07 15:55:00 -0700

    Remove local error package from imports

--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,4 @@
 # Import the create_cd_account and create_savings_account functions
-import pretty_errors
 from cd_account import create_cd_account
 from savings_account import create_savings_account
 
